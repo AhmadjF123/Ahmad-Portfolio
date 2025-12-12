28a4dbd497e6beaae29200fb67847f7056a059d1
Ahmad Farroukh <ahmdf2855@gmail.com>
2025-12-12 13:12:04 +0300

    Updating My CV

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -15,7 +15,7 @@ function HeroSection() {
 
         <div>
           <a
-            href="/AhmadFarroukhCV.pdf"
+            href="/AhmadJibrilFarroukhCV.pdf"
             className="inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
             target="_blank"
             rel="noopener noreferrer"
